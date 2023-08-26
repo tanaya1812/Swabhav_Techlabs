a68d0415a83ed36e73fcd0fa5b2fcf667ddade66
Tanaya Raikwar <Tanaya.Raikwar@asl.mum.sst>
2023-08-26 15:24:29 +0530

    v2 exceptions handled

--- JavaScript/tictactoe/Game.js
+++ JavaScript/tictactoe/Game.js
@@ -3,67 +3,79 @@ var Player = require('./Player.js');
 
 
 class Game {
-
+    #board
+    #players
+    #turn
+    #isGameEnded
     constructor(player0Name, player1Name) {
-        this.board = new Board()
-        this.players = [
+        this.#board = new Board()
+        this.#players = [
             new Player(player0Name, 'X'),
             new Player(player1Name, 'O')
         ];
 
-        this.turn = 0
-        this.isGameEnded = false
+        this.#turn = 0
+        this.#isGameEnded = false
     }
 
     static newGame(player0Name, player1Name) {
-        if (player0Name == player1Name || typeof player0Name != 'string' || typeof player1Name != 'string') {
-            return ['Invalid name', null]
+        try {
+            if (player0Name == player1Name || typeof player0Name != 'string' || typeof player1Name != 'string') {
+                throw new Error("Invalid name")
+            }
+            return ['Object created', new Game(player0Name, player1Name)]
+        } catch (error) {
+            console.log(error.message)
         }
-        return ['Object created', new Game(player0Name, player1Name)]
+        
+        
     }
 
 
     play(cellNumber) {
-        //validation of cell Number
+        try {
+            //validation of cell Number
         if (cellNumber < 0 || cellNumber > 8 || typeof cellNumber != 'number') {
-            
-            return 'Invalid Cell Number'
+            throw new Error("Invalid Cell Number")
         }
 
-
-        if (this.isGameEnded) {
+        if (this.#isGameEnded) {
             return 'Game has Ended'
         }
 
-        if (!this.board.isCellEmpty(cellNumber)) {
-            return 'Cell not empty'
+        if (!this.#board.isCellEmpty(cellNumber)) {
+            return 'Cell is not empty'
         }
 
         let currentPlayer = null
 
-        if (this.turn % 2 == 0) {
-            currentPlayer = this.players[0]
+        if (this.#turn % 2 == 0) {
+            currentPlayer = this.#players[0]
         }
         else {
-            currentPlayer = this.players[1]
+            currentPlayer = this.#players[1]
         }
         
-        this.board.markCell(cellNumber, currentPlayer.getSymbol())
-        this.turn++
+        this.#board.markCell(cellNumber, currentPlayer.getSymbol())
+        this.#turn++
         
-        this.board.printBoard()
+        this.#board.printBoard()
         
-        if(this.board.resultAnalyzer()){
-            this.isGameEnded = true;
+        if(this.#board.resultAnalyzer()){
+            this.#isGameEnded = true;
             return `${currentPlayer.name} Wins!`
         }
         
-        if (this.board.isDraw()) {
-            this.isGameEnded = true;
+        if (this.#board.isDraw()) {
+            this.#isGameEnded = true;
             return "Game is Draw";
         }
         
         return "Continue.."
+        } catch (error) {
+            console.log(error.message)
+        }
+        
     
     }
 }
